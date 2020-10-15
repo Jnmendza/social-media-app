a304b232f57a20abcbc2f8d968ac27cc99cd0afa
Jnmendza <jonatmendoza@gmail.com>
2020-10-15 01:58:45 -0700

    BE complete

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,59 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
+
+module.exports = {
+    Mutation: {
+        createComment: async (_, { postId, body }, context) => {
+            // First make sure we have a valid user by checking Auth
+            const { username } = checkAuth(context);
+            if(body.trim() === ''){
+                // body of the comment cannot by empty
+                throw new UserInputError('Empty comment', {
+                    errors: {
+                        body: 'Comment body must not be empty'
+                    }
+                })
+            }
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                post.comments.unshift({
+                    body,
+                    username,
+                    createdAt: new Date().toISOString()
+                })
+                await post.save()
+                return post;
+            } else throw new UserInputError('Post not found');
+        },
+        deleteComment: async (_, { postId, commentId }, context) => {
+            // First make sure we have a valid user by checking Auth
+            const { username } = checkAuth(context);
+            // Then find the post with the specific Id
+            const post = await Post.findById(postId);
+
+            // We'll find the comment in the array of comments so we have to locate the index in comments array
+            if(post){
+                // Find the idx of the comment by matching the comment's ID
+                const commentIndex = post.comments.findIndex(comment => comment.id === commentId);
+
+                // If the username of the post matches the username from checkAuth...
+                if(post.comments[commentIndex].username === username){
+                    // ... remove it from the array with splice and using the commentIdx to locate it
+                    post.comments.splice(commentIndex, 1);
+                    // with the new array updated without the comment, save it to the db
+                    await post.save();
+                    // return the new post array
+                    return post;
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } else {
+                throw new UserInputError('Post not found!')
+            }
+        }
+    }
+}
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,96 @@
-const Post = require('../../models/Post')
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts
             } catch(err){
                 throw new Error(err)
             }
+        },
+        async getPost(_, { postId }){
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch(err){
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context){
+            const user = checkAuth(context);
+            console.log(user)
+
+            const newPost = new Post({
+                body,
+                user: user.indexOf,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            })
+
+            return post;
+        },
+        async deletePost(_, { postId }, context){
+            const user = checkAuth(context);
+
+            try{
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.deleteOne();
+                    return 'Post deleted successfully'
+                } else {
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } catch(err){
+                throw new Error(err)
+            }
+        },
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+            // Find the post with the postId
+            const post = await Post.findById(postId);
+            // If its the correct post
+            if(post){
+                // Check to see if the like is from the correct user by checking username
+                if(post.likes.find(like => like.username === username)){
+                    // Post already liked, unlike it -- Add an unlike
+                    // update the likes with JUST the likes that are not from the given username
+                    post.likes = post.likes.filter(like => like.username !== username);
+                    
+                } else {
+                    // Not liked, like post -- Add a like
+                    // Add the newly created Like with the proper fields
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+
+                // save it to the db
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 }
